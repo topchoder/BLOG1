55b487f4e76a685a1002c826482adba9478ac32d
Anurag Maurya <anuragmauryag@gmail.com>
2024-10-09 09:43:32 -0700

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.urls import path
+from .views import blog_list, blog_detail
+
+urlpatterns = [
+    path('', blog_list, name='blog_list'),
+    path('blog/<int:blog_id>/', blog_detail, name='blog_detail'),
+]
+
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
