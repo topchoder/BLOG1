502430fbe4534cc5b7a92cd48b08f36e9c76e53d
Anurag Maurya <anuragmauryag@gmail.com>
2024-10-09 09:45:56 -0700

    Update views.py

--- views.py
+++ views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
 from django.core.paginator import Paginator
+def add_comment(request, blog_id):
+    if request.method == 'POST':
+        blog = get_object_or_404(Blog, id=blog_id)
+        comment = Comment.objects.create(
+            blog=blog,
+            user=request.user,
+            content=request.POST['content']
+        )
+        return redirect('blog_detail', blog_id=blog.id)
+
 
 def blog_list(request):
     blogs = Blog.objects.all().order_by('-created_at')
